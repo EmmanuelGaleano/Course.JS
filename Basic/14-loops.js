fb886afae52c589367f5ffa142df87df06362d69
EmmanuelGaleano <emmanuelgaleano013@gmail.com>
2025-10-17 22:13:20 -0500

    Full Loops

--- Basic/14-loops.js
+++ Basic/14-loops.js
@@ -2,6 +2,80 @@
 
 // for
 
-for (let i = 0; i < 13; i++) {
-    console.log('hola ${}');
+for (let i = 0; i < 5; i++) {
+    console.log(`hola ${i}`);
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(`hola numero: ${numbers[i]}`);
+}
+
+// while
+
+let i = 0;
+
+while (i > 5) {
+console.log(`hola ${i}`);
+i++;
+}
+
+// infinite loop
+// while (true) {
+
+// }
+
+// do...while
+
+
+ i = 6;
+do {
+    console.log(`hola ${i}`);
+    i++;
+}while (i < 5);
+
+// for of
+
+myArray = [1, 2, 3, 4];
+
+mySet = new Set(['Emmanuel', 'Gale', 'Galeano', 18, true, 'emmanuelgaleano013@hgmail.com']);
+
+myMap = new Map([
+    ['name', 'Emmanuel'],
+    ['lastname', 'Galeano'],
+    ['age', 18],
+    ['email', 'emmanuelgaleano013@gmail.com'],
+]);
+
+
+for (let valoe of myArray) {
+    console.log(valoe)
+}
+
+for (let valor of mySet) {
+    console.log(valor)
+}
+
+for (let [key, value] of myMap) {
+    console.log(`${key}: ${value}`);
+}
+
+myString = "Hello";
+
+for (let char of myString) {
+    console.log(char);
+}
+
+// good practice
+
+// break and continue
+
+for (let i = 0; i < 10; i++){
+    if (i == 5){
+        continue
+    } else if (i == 9) {
+        break
+    }
+    console.log(`Hola ${i}`)
 }
---
+++ Basic/15-loopsExcercises.js
@@ -0,0 +1,101 @@
+// loops Exercises
+
+
+//#1
+
+for (let i = 1; i <= 20; i++) {
+  console.log(`Numero ${i}`);
+}
+
+//#2
+
+let suma = 0;
+for (let i = 1; i <= 100; i++) {
+  suma += i;
+}
+console.log("La suma es:", suma);
+
+//#3
+
+for (let i = 2; i <= 50; i += 2) {
+  console.log(i);
+}
+
+//#4
+
+let nombres = ["Ana", "Luis", "Carlos", "Sofía"];
+let j = 0;
+
+while (j < nombres.length) {
+  console.log(nombres[j]);
+  j++;
+}
+
+//#5
+
+let texto = "Hola Mundo";
+let vocales = "aeiouAEIOU";
+let contador = 0;
+let k = 0;
+
+while (k < texto.length) {
+  if (vocales.includes(texto[k])) {
+    contador++;
+  }
+  k++;
+}
+
+console.log("Número de vocales:", contador);
+
+//#6
+
+let numeros = [2, 3, 4, 5];
+let producto = 1;
+let x = 0;
+
+while (x < numeros.length) {
+  producto *= numeros[x];
+  x++;
+}
+
+console.log("Producto total:", producto);
+
+let a = 1;
+
+//#7
+
+
+do {
+  console.log(`5 x ${a} = ${5 * a}`);
+  a++;
+} while (a <= 10);
+
+let cadena = "JavaScript";
+let invertida = "";
+let b = cadena.length - 1;
+
+//#8
+
+
+do {
+  invertida += cadena[b];
+  b--;
+} while (b >= 0);
+
+console.log("Cadena invertida:", invertida);
+
+//#9
+
+let n1 = 0, n2 = 1, siguiente;
+let c = 3;
+
+console.log(n1);
+console.log(n2);
+
+do {
+  siguiente = n1 + n2;
+  console.log(siguiente);
+  n1 = n2;
+  n2 = siguiente;
+  c++;
+} while (c <= 10);
